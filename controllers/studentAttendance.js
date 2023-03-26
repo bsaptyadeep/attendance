9898b1cc3f74cc6dc2cceedfcce0c58197109c6e
Saptyadeep <bsaptyadeep@gmail.com>
2023-03-26 18:55:52 +0530

    Added mentor to attendance model and year and time as string

--- controllers/studentAttendance.js
+++ controllers/studentAttendance.js
@@ -1,5 +1,6 @@
 const Attendance = require("../models/attendanceModel");
 const mongoose = require('mongoose');
+const { time } = require("console");
 require("dotenv").config();
 
 const studentAttendance = async (req, res) => {
@@ -9,15 +10,20 @@ const studentAttendance = async (req, res) => {
         let date = date_ob.getDate();
         let month = date_ob.getMonth() + 1;
         let year = date_ob.getFullYear();
+        // time in HH:MM:SS
+        let time_str = `${date_ob.getHours()}:${date_ob.getMinutes()}:${date_ob.getSeconds()}`;
+        // date in MM-DD-YYYY
         const date_str = `${month}-${date}-${year}`
         const attendance = new Attendance({
             name: req.body.name,
             branch: req.body.branch,
             year: req.body.year,
+            mentor: req.query.mentor,
             date: date_str,
-            subject: req.body.subject
+            time: time_str,
+            subject: req.query.subject
         })
-
+        console.log('post-attendance', attendance);
         await attendance.save()
         res.status(201).send('Attendance submitted, Successfully');
     } catch (error) {
@@ -31,8 +37,9 @@ const getallStudentAttendance = async (req, res, next) => {
         var name = req.query.name;
         var date = req.query.date;
         var subject = req.query.subject;
+        var mentor = req.query.mentor;
         let attendance = await Attendance.find();
-        console.log(name, date, subject)
+        console.log('get attendance for ---> name: ', name,'date: ', date, 'subject: ', subject, 'mentor: ', mentor);
         if (name)
             attendance = attendance.filter((ob) => {
                 return name == ob.name
@@ -44,6 +51,10 @@ const getallStudentAttendance = async (req, res, next) => {
         if (subject)
             attendance = attendance.filter((ob) => {
                 return subject == ob.subject
+            })
+        if (mentor)
+            attendance = attendance.filter((ob) => {
+                return mentor == ob.mentor
             })
         res.status(200).send(attendance);
     } catch (error) {
--- models/attendanceModel.js
+++ models/attendanceModel.js
@@ -5,7 +5,9 @@ const Schema = mongoose.Schema;
 const attendanceSchema = new Schema({
     name: { type: String, required: true },
     branch: { type: String, required: true },
-    year: { type: Number, required: true },
+    mentor: { type: String, required: true },
+    year: { type: String, required: true },
+    time: { type: String, required: true },
     date: { type: String, required: true },
     subject: { type: String, require: true}
 }, { timestamps: true });
